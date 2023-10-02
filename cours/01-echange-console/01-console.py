f897e46b65224c5fc792f6530d2df19a4d903c2b
QBidolet <quentin.bidolet@gmail.com>
2023-10-02 12:08:54 +0200

    - commentaires

---
+++ cours/01-echange-console/01-console.py
@@ -0,0 +1,8 @@
+# Afficher la version de Python
+# python --version
+
+# Afficher la liste des bibliothèques (packages)
+# pip list
+
+# Zen de Python
+import this
---
+++ cours/02-premier-programme/01-comment.py
@@ -0,0 +1,14 @@
+# Un commentaire simple.
+
+# Plusieurs lignes
+# Une autre
+
+"""
+Une ligne
+Une autre
+etc.
+
+C'est un DOCSTRING.
+Peut être utilisé pour décrire des fonctions,
+méthodes, classes et fichiers.
+"""
